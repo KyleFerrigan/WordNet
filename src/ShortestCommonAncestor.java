97f3a972a9b8fb956d5f7163da7cc08ed46bd1ca
pagalati <78822760+pagalati@users.noreply.github.com>
2021-04-16 14:51:34 -0500

    Update ShortestCommonAncestor.java
    Added functionality to length and ancestor

--- src/ShortestCommonAncestor.java
+++ src/ShortestCommonAncestor.java
@@ -11,16 +11,50 @@ public class ShortestCommonAncestor {
         // Check to see if either vertex are in range, throw exception if they are outside the prescribed range
         if (v < 0 || v > DAG.V()) throw new java.lang.IllegalArgumentException("v is out of range");
         if (w < 0 || w > DAG.V()) throw new java.lang.IllegalArgumentException("w is out of range");
-        //todo breadth first search
-
-        return -1; //todo change
+        
+        // Create two graphs for the two vertices
+        dag1 = new BreadthFirstDirectedPaths(DAG, v);
+        dag2 = new BreadthFirstDirectedPaths(DAG, w);
+        
+        int vertex = DAG.V();
+        int minLength = Integer.MAX_VALUE;
+        // Go through the vertices and measure out the length, update the length whenever the distance (edges) to the two graphs are greater than the length
+        for (int i = 0; i < vertex; i++){
+            if (dag1.hasPathTo(i) && dag2.hasPathTo(i)){
+                if (minLength > dag1.distTo(i) + dag2.distTo(i)) {
+                    minLength = dag1.distTo(i) + dag2.distTo(i);
+                }
+            }
+        }
+        // If the length never changed, then the vertex is its own ancestor, otherwise return the length
+        if (minLength == Integer.MAX_VALUE) return -1;
+        return minLength;
     }
     // a shortest common ancestor of vertices v and w
     public int ancestor(int v, int w){
         // Check to see if either vertex are in range, throw exception if they are outside the prescribed range
         if (v < 0 || v > DAG.V()) throw new java.lang.IllegalArgumentException("v is out of range");
         if (w < 0 || w > DAG.V()) throw new java.lang.IllegalArgumentException("w is out of range");
-        return -1; //todo change
+          
+        // Create two graphs for the two vertices
+        dag1 = new BreadthFirstDirectedPaths(DAG, v);
+        dag2 = new BreadthFirstDirectedPaths(DAG, w);
+        
+        int vertex = DAG.V();
+        int minLength = Integer.MAX_VALUE;
+        int result = -1;
+        // Go through the vertices and measure out the length, update the length whenever the distance (edges) to the two graphs are greater than the length
+        // Store the number into result
+        for (int i = 0; i < vertex; i++){
+            if (dag1.hasPathTo(i) && dag2.hasPathTo(i)){
+                if (minLength > dag1.distTo(i) + dag2.distTo(i)){
+                    minLength = dag1.distTo(i) + dag2.distTo(i);
+                    result = i;
+                }
+            }
+        }
+        // If the length never changed, then the vertex is its own ancestor (-1), otherwise it is the updated length
+        return result;
     }
     // length of shortest ancestral path of vertex subsets A and B
     public int lengthSubset(Iterable<Integer> subsetA, Iterable<Integer> subsetB){

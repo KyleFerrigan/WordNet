c42b69bb46cbf6c7378c4d5b2bcbb1dc27e1bc8c
Kyle Ferrigan <kbferrigan@outlook.com>
2021-04-16 15:59:08 -0500

    Added to Wordnet

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,24 +1,33 @@
+import java.util.*;
+
 public class WordNet {
+    HashMap hMap = new HashMap<Integer, String>();
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms){
+        Scanner scan = new Scanner();
 
     }
+
     // the set of all WordNet nouns
     public Iterable<String> nouns(){
         return null; //todo change
     }
+
     // is the word a WordNet noun?
     public boolean isNoun(String word){
         return false;//todo change
     }
+
     // a synset (second field of synsets.txt) that is a shortest common ancestor // of noun1 and noun2 (defined below)
     public String sca(String noun1, String noun2){
         return null;//todo change
     }
+
     // distance between noun1 and noun2 (defined below)
     public int distance(String noun1, String noun2){
         return -1;//todo change
     }
+
     // unit testing (required)
     public static void main(String[] args){
 

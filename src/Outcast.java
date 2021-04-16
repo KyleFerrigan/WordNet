3eeb9f012f9c28f0fa67a96720ab65c74c114434
Kyle Ferrigan <kbferrigan@outlook.com>
2021-04-16 13:43:37 -0500

    init

---
+++ src/Outcast.java
@@ -0,0 +1,14 @@
+public class Outcast {
+    // constructor takes a WordNet object
+    public Outcast(WordNet wordnet){
+
+    }
+    // given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns){
+        return null; //todo change
+    }
+    // test client (see below)
+    public static void main(String[] args){
+
+    }
+}

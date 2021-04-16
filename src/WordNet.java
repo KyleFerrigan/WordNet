0747c30af55f471bc36cedb6ffd60ee89318e1f0
pagalati <78822760+pagalati@users.noreply.github.com>
2021-04-16 15:57:53 -0500

    Update WordNet.java
    Added IllegalArgumentExceptions to methods

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,6 +1,8 @@
 public class WordNet {
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms){
+        if (synsets == null) throw new java.lang.IllegalArgumentException("synsets is null");
+        if (hypernyms == null) throw new java.lang.IllegalArgumentException("hypernyms is null");
 
     }
     // the set of all WordNet nouns
@@ -9,14 +11,19 @@ public class WordNet {
     }
     // is the word a WordNet noun?
     public boolean isNoun(String word){
+        if (word == null) throw new java.lang.IllegalArgumentException(word + " is null");
         return false;//todo change
     }
     // a synset (second field of synsets.txt) that is a shortest common ancestor // of noun1 and noun2 (defined below)
     public String sca(String noun1, String noun2){
+        if (noun1 == null) throw new java.lang.IllegalArgumentException(noun1 + " is null");
+        if (noun2 == null) throw new java.lang.IllegalArgumentException(noun2 + " is null");
         return null;//todo change
     }
     // distance between noun1 and noun2 (defined below)
     public int distance(String noun1, String noun2){
+        if (noun1 == null) throw new java.lang.IllegalArgumentException(noun1 + " is null");
+        if (noun2 == null) throw new java.lang.IllegalArgumentException(noun2 + " is null");
         return -1;//todo change
     }
     // unit testing (required)
